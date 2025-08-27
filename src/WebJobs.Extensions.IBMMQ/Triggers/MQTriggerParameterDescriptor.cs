4c0ced7c433c92a9cf58aaa5e53e854926befcb8
baidarka <baidarka@users.noreply.github.com>
2025-08-27 14:29:05 +0000

    debugger display

--- src/WebJobs.Extensions.IBMMQ/Triggers/MQTriggerParameterDescriptor.cs
+++ src/WebJobs.Extensions.IBMMQ/Triggers/MQTriggerParameterDescriptor.cs
@@ -1,11 +1,18 @@
+using System.Diagnostics;
 using System.Text.Json;
 using Microsoft.Azure.WebJobs.Host.Protocols;
 
 namespace Azure.WebJobs.Extensions.IBMMQ.Triggers;
 
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
 internal class MQTriggerParameterDescriptor : TriggerParameterDescriptor
 {
     public string QueueName { get; }
+
+    private string GetDebuggerDisplay()
+    {
+        return $"QueueName = {QueueName}";
+    }
 
     public MQTriggerParameterDescriptor(string queueName)
     {
